e3895f4fc256a2a955b551714216a3246c3e5ae6
jukkty <dima.iachimov@gmail.com>
2021-03-15 14:51:54 +0200

    final commit i hope

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        boolean enoughMoneyForBonuses = true;
-        if (enoughMoneyForBonuses) {
-            int transfer = 2700;
+        int transfer = 2700;
+        int initialWallet = 500;
+        if (transfer > 1000) {
             int bonus = 100;
-            int initialWallet = 500;
             int finalBonus = transfer / bonus;
             int finalBalance = finalBonus + initialWallet + transfer;
             System.out.println("Поздравляем,вам начислено "+finalBonus+ " рублей");
