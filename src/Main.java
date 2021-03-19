01fb6c0dd6c57858d024ad06001cea96ffff42c2
jukkty <dima.iachimov@gmail.com>
2021-03-19 09:27:59 +0200

    No more commits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+//        Первый вариант сделал через if и else if:
         int transfer = 2700;
         int initialWallet = 500;
         if (transfer > 1000) {
@@ -17,23 +18,24 @@ public class Main {
         }
     }
 }
-public class Main {
-    public static void main(String[] args) {
-        int transfer = 2700;
-        int transferNeededForBonuses = 1000;
-        int initialWallet = 500;
-        int coefficient = 100;
-        boolean bonusAdded = (transfer > transferNeededForBonuses);
-        if (bonusAdded) {
-            int bonus = transfer / coefficient;
-            int balanceWithBonus = bonus + initialWallet + transfer;
-            System.out.println("Поздравляем,вам начислено "+bonus+" бонусных рублей");
-            System.out.println("Поздравляем,ваш итоговый баланс составляет "+balanceWithBonus+" рублей");
-        }
-        else {
-            int balanceWithoutBonus = initialWallet + transfer;
-            System.out.println("Поздравляем,вам начислено "+balanceWithoutBonus+" рублей");
-            System.out.println("Для начисления бонусов,необходимо пополнить счет на сумму более 1000 рублей");
-        }
-    }
-}
+//Второй вариант через boolean и проверку на то хватает ли трансферных денег для начисления бонусов или нет:
+// public class Main {
+//    public static void main(String[] args) {
+//        int transfer = 2700;
+//        int transferNeededForBonuses = 1000;
+//        int initialWallet = 500;
+//        int coefficient = 100;
+//        boolean bonusAdded = (transfer > transferNeededForBonuses);
+//        if (bonusAdded) {
+//            int bonus = transfer / coefficient;
+//            int balanceWithBonus = bonus + initialWallet + transfer;
+//            System.out.println("Поздравляем,вам начислено "+bonus+" бонусных рублей");
+//            System.out.println("Поздравляем,ваш итоговый баланс составляет "+balanceWithBonus+" рублей");
+//        }
+//        else {
+//            int balanceWithoutBonus = initialWallet + transfer;
+//            System.out.println("Поздравляем,вам начислено "+balanceWithoutBonus+" рублей");
+//            System.out.println("Для начисления бонусов,необходимо пополнить счет на сумму более 1000 рублей");
+//        }
+//    }
+//}
